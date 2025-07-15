51498d4be5de2885dbce0a9e33b6d8ac3317de18
Shihab Mahamud <shihab4t@gmail.com>
2025-07-16 00:52:59 +0600

    center text on screen

--- main.go
+++ main.go
@@ -1,9 +1,21 @@
 package main
 
 import (
-	"fmt"
+	"github.com/rivo/tview"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	app := tview.NewApplication()
+
+	text := tview.NewTextView().
+		SetText("Centered text").
+		SetTextAlign(tview.AlignCenter)
+
+	root := tview.NewFlex().
+		SetDirection(tview.FlexRow).
+		AddItem(nil, 0, 1, false).
+		AddItem(text, 1, 0, false).
+		AddItem(nil, 0, 1, false)
+
+	app.SetRoot(root, true).Run()
 }
