5c03df22279edde0e6c9f895c93c0120ae886a36
Shihab Mahamud <shihab4t@gmail.com>
2025-07-16 01:07:50 +0600

    go hello world

--- main.go
+++ main.go
@@ -1,21 +1,7 @@
 package main
 
-import (
-	"github.com/rivo/tview"
-)
+import "fmt"
 
 func main() {
-	app := tview.NewApplication()
-
-	text := tview.NewTextView().
-		SetText("Centered text").
-		SetTextAlign(tview.AlignCenter)
-
-	root := tview.NewFlex().
-		SetDirection(tview.FlexRow).
-		AddItem(nil, 0, 1, false).
-		AddItem(text, 1, 0, false).
-		AddItem(nil, 0, 1, false)
-
-	app.SetRoot(root, true).Run()
+	fmt.Println("hello world")
 }
