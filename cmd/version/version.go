64949c2bda784750bd0990ffa3fcc80745bb1aa1
Shihab Mahamud <shihab4t@gmail.com>
2025-07-16 08:59:06 +0600

    update release workflow

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -1,7 +1,10 @@
 // Package version
 package version
 
-import "fmt"
+import (
+	"fmt"
+	"runtime"
+)
 
 var (
 	Version = "dev"
@@ -10,7 +13,11 @@ var (
 )
 
 func Run(args []string) error {
-	fmt.Printf("termodoro version %s\n", Version)
+	arch := runtime.GOARCH
+	if arch == "amd64" {
+		arch = "x86_64"
+	}
+	fmt.Printf("termodoro version %s %s/%s\n", Version, runtime.GOOS, arch)
 	if Commit != "unknown" {
 		fmt.Printf("commit: %s\n", Commit)
 	}
