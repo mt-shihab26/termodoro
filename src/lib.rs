cc2c76c5916d1c169c8edc59f5175cdac2d937c1
Shihab Mahamud <shihab4t@gmail.com>
2026-03-26 18:05:07 +0600

    remove errors and loggings

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,4 @@ pub mod app;
 pub mod cli;
 pub mod components;
 pub mod config;
-pub mod errors;
-pub mod logging;
 pub mod tui;
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 use clap::Parser;
-use termodoro::{app::App, cli::Cli, errors, logging};
+use termodoro::{app::App, cli::Cli};
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
-    errors::init()?;
-    logging::init()?;
-
     let args = Cli::parse();
     let mut app = App::new(args.tick_rate, args.frame_rate)?;
     app.run().await?;
