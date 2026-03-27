0e986a7bd2a04971729a8efdf0b58c5996b0dec5
Shihab Mahamud <shihab4t@gmail.com>
2026-03-27 12:54:45 +0600

    version

--- src/cmd/version.rs
+++ src/cmd/version.rs
@@ -1,9 +1,12 @@
+use std::io::Result;
+
 use crate::cmd::Cmd;
 
 pub struct Version;
 
 impl Cmd for Version {
-    fn run() -> std::io::Result<()> {
-        todo!()
+    fn run() -> Result<()> {
+        println!("{}", env!("CARGO_PKG_VERSION"));
+        Ok(())
     }
 }
