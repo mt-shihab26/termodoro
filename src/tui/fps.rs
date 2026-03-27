a3b4c09c66d64bb082f58d4776b0939d7e1648e1
Shihab Mahamud <shihab4t@gmail.com>
2026-03-27 16:48:10 +0600

    fps

--- src/tui/fps.rs
+++ src/tui/fps.rs
@@ -4,7 +4,7 @@ pub struct Fps {
     pub per_second: f64,
     pub per_lifetime: u64,
     frame_count_per_second: u32,
-    timer: Instant,
+    interval_start: Instant,
 }
 
 impl Fps {
@@ -13,7 +13,7 @@ impl Fps {
             per_second: 0.0,
             per_lifetime: 0,
             frame_count_per_second: 0,
-            timer: Instant::now(),
+            interval_start: Instant::now(),
         }
     }
 
@@ -22,11 +22,11 @@ impl Fps {
 
         self.frame_count_per_second += 1;
 
-        let elapsed = self.timer.elapsed().as_secs_f64();
+        let elapsed = self.interval_start.elapsed().as_secs_f64();
         if elapsed >= 1.0 {
             self.per_second = self.frame_count_per_second as f64 / elapsed;
             self.frame_count_per_second = 0;
-            self.timer = Instant::now();
+            self.interval_start = Instant::now();
         }
     }
 }
