81a29e28e90eb81bf68a60fc36944f23f57cab87
Shihab Mahamud <shihab4t@gmail.com>
2026-03-27 21:23:35 +0600

    timer worker

--- src/workers/timer_worker.rs
+++ src/workers/timer_worker.rs
@@ -9,7 +9,7 @@ pub const WORK_DURATION: u64 = 25 * 60 * 1000;
 pub const BREAK_DURATION: u64 = 5 * 60 * 1000;
 pub const LONG_BREAK_DURATION: u64 = 15 * 60 * 1000;
 pub const LONG_BREAK_INTERVAL: u32 = 4;
-pub const SHOW_MILLIS: bool = false;
+pub const SHOW_MILLIS: bool = true;
 
 fn tick_interval() -> u64 {
     if SHOW_MILLIS { 10 } else { 1000 }
