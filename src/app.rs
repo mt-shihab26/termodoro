f10affee91c9a6b84f030901fad38e426c3bf3c6
Shihab Mahamud <shihab4t@gmail.com>
2026-03-27 12:14:54 +0600

    app

--- src/app.rs
+++ src/app.rs
@@ -1,18 +1,47 @@
 use std::io::Result;
 
 use ratatui::DefaultTerminal;
+use ratatui::crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use ratatui::{Frame, widgets::Paragraph};
 
-pub struct App {
+pub struct App<'a> {
     alive: bool,
-    terminal: &mut DefaultTerminal,
+    terminal: &'a mut DefaultTerminal,
 }
 
-impl App {
-    pub fn new(terminal: &mut DefaultTerminal) -> Self {
+impl<'a> App<'a> {
+    pub fn new(terminal: &'a mut DefaultTerminal) -> Self {
         Self { alive: true, terminal }
     }
 
-    pub fn run() -> Result<()> {
+    pub fn run(&mut self) -> Result<()> {
+        while self.alive {
+            self.handle_events()?;
+            self.render_pixels()?;
+        }
+
         Ok(())
+    }
+
+    fn handle_events(&mut self) -> Result<()> {
+        match event::read()? {
+            Event::Key(key) if key.kind == KeyEventKind::Press => match key.code {
+                KeyCode::Char('q') => return Ok(()),
+                _ => {}
+            },
+            _ => {}
+        }
+        Ok(())
+    }
+
+    fn render_pixels(&mut self) -> Result<()> {
+        self.terminal.draw(|frame| self.render_frame(frame))?;
+
+        Ok(())
+    }
+
+    fn render_frame(&mut self, frame: &mut Frame) {
+        let text = Paragraph::new("Hello World!");
+        frame.render_widget(text, frame.area());
     }
 }
