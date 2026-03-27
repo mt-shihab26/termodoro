e48a31e937339f06db942458bbedc131264bec8e
Shihab Mahamud <shihab4t@gmail.com>
2026-03-27 14:45:35 +0600

    app

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -1,20 +1,25 @@
 mod tabs;
-mod ui;
 
 use std::io::Result;
 use std::time::Duration;
 
 use ratatui::DefaultTerminal;
+use ratatui::buffer::Buffer;
 use ratatui::crossterm::event::{self, Event, KeyCode, KeyEventKind};
+use ratatui::layout::{Constraint, Layout, Rect};
+use ratatui::style::{Color, Style, Stylize};
+use ratatui::symbols;
+use ratatui::text::{Line, Span};
+use ratatui::widgets::{Tabs, Widget};
 
-use self::tabs::timer::TimerState;
-use self::ui::Ui;
+use self::tabs::timer::{self, Timer, TimerState};
+use self::tabs::todos::{self, Todos};
 
 pub struct App<'a> {
-    alive: bool,
-    terminal: &'a mut DefaultTerminal,
+    alive:        bool,
+    terminal:     &'a mut DefaultTerminal,
     selected_tab: usize,
-    timer: TimerState,
+    timer:        TimerState,
 }
 
 impl<'a> App<'a> {
@@ -38,16 +43,18 @@ impl<'a> App<'a> {
     fn handle_events(&mut self) -> Result<()> {
         if event::poll(Duration::from_secs(1))? {
             match event::read()? {
-                Event::Key(key) if key.kind == KeyEventKind::Press => match key.code {
-                    KeyCode::Char('q') => self.alive = false,
-                    KeyCode::Char('1') => self.selected_tab = 0,
-                    KeyCode::Char('2') => self.selected_tab = 1,
-                    _ => {
-                        if self.selected_tab == 1 {
-                            self.timer.handle_event(key);
+                Event::Key(key) if key.kind == KeyEventKind::Press => {
+                    match key.code {
+                        KeyCode::Char('q') => self.alive = false,
+                        KeyCode::Char('1') => self.selected_tab = 0,
+                        KeyCode::Char('2') => self.selected_tab = 1,
+                        _ => {
+                            if self.selected_tab == 1 {
+                                self.timer.handle_event(key);
+                            }
                         }
                     }
-                },
+                }
                 _ => {}
             }
         } else {
@@ -63,5 +70,43 @@ impl<'a> App<'a> {
             frame.render_widget(Ui { selected_tab, timer }, frame.area());
         })?;
         Ok(())
+    }
+}
+
+struct Ui<'a> {
+    selected_tab: usize,
+    timer:        &'a TimerState,
+}
+
+impl<'a> Widget for Ui<'a> {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let [top, tabs_area, main] = Layout::vertical([
+            Constraint::Length(1),
+            Constraint::Length(1),
+            Constraint::Fill(1),
+        ]).areas(area);
+
+        Line::from_iter([Span::from("Orivo").bold().fg(Color::Green)])
+            .centered()
+            .render(top, buf);
+
+        let highlight_color = match self.selected_tab {
+            0 => todos::COLOR,
+            _ => timer::COLOR,
+        };
+
+        Tabs::new(vec!["Todos", "Timer"])
+            .style(Color::White)
+            .highlight_style(Style::default().fg(highlight_color).on_black().bold())
+            .select(self.selected_tab)
+            .divider(symbols::DOT)
+            .padding(" ", " ")
+            .render(tabs_area, buf);
+
+        match self.selected_tab {
+            0 => Todos.render(main, buf),
+            1 => Timer::new(self.timer).render(main, buf),
+            _ => unreachable!(),
+        }
     }
 }
