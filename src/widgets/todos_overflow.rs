7f290730e755aa5c8a1ac5dc991a627d85072205
Shihab Mahamud <shihab4t@gmail.com>
2026-04-01 02:48:54 +0600

    todos

--- src/widgets/todos_overflow.rs
+++ src/widgets/todos_overflow.rs
@@ -1,7 +1,6 @@
-use ratatui::Frame;
-use ratatui::layout::Rect;
-use ratatui::style::{Color, Stylize};
-use ratatui::widgets::Paragraph;
+use ratatui::{Frame, layout::Rect, style::Stylize, widgets::Paragraph};
+
+use crate::tabs::todos::COLOR;
 
 pub struct TodosOverflowWidget {
     pub show_more_above: bool,
@@ -11,14 +10,11 @@ pub struct TodosOverflowWidget {
 impl TodosOverflowWidget {
     pub fn render(self, frame: &mut Frame, top_area: Rect, bottom_area: Rect) {
         if self.show_more_above && top_area.height > 0 {
-            frame.render_widget(Paragraph::new("^ more").fg(Color::DarkGray), top_area);
+            frame.render_widget(Paragraph::new("^ more").fg(COLOR).right_aligned(), top_area);
         }
 
         if self.show_more_below && bottom_area.height > 0 {
-            frame.render_widget(
-                Paragraph::new("v more").fg(Color::DarkGray).right_aligned(),
-                bottom_area,
-            );
+            frame.render_widget(Paragraph::new("v more").fg(COLOR).right_aligned(), bottom_area);
         }
     }
 }
