364110d7db52f7c8107d993cb56d170a2207ea85
Shihab Mahamud <shihab4t@gmail.com>
2026-03-31 21:47:45 +0600

    timer

--- src/config/timer.rs
+++ src/config/timer.rs
@@ -2,44 +2,26 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Deserialize, Serialize)]
 pub struct TimerConfig {
-    #[serde(default = "TimerConfig::default_show_millis")]
+    #[serde(default)]
     pub show_millis: bool,
-    #[serde(default = "TimerConfig::default_work_duration")]
+    #[serde(default)]
     pub work_duration: u64,
-    #[serde(default = "TimerConfig::default_break_duration")]
+    #[serde(default)]
     pub break_duration: u64,
-    #[serde(default = "TimerConfig::default_long_break_duration")]
+    #[serde(default)]
     pub long_break_duration: u64,
-    #[serde(default = "TimerConfig::default_long_break_interval")]
+    #[serde(default)]
     pub long_break_interval: u64,
-}
-
-impl TimerConfig {
-    fn default_show_millis() -> bool {
-        true
-    }
-    fn default_work_duration() -> u64 {
-        25
-    }
-    fn default_break_duration() -> u64 {
-        5
-    }
-    fn default_long_break_duration() -> u64 {
-        15
-    }
-    fn default_long_break_interval() -> u64 {
-        4
-    }
 }
 
 impl Default for TimerConfig {
     fn default() -> Self {
         Self {
-            show_millis: Self::default_show_millis(),
-            work_duration: Self::default_work_duration(),
-            break_duration: Self::default_break_duration(),
-            long_break_duration: Self::default_long_break_duration(),
-            long_break_interval: Self::default_long_break_interval(),
+            show_millis: true,
+            work_duration: 25,
+            break_duration: 5,
+            long_break_duration: 15,
+            long_break_interval: 4,
         }
     }
 }
