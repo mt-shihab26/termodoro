e04b1ab692ddfd799d5ab9e2569aaff99b0d74b2
Shihab Mahamud <shihab4t@gmail.com>
2026-04-05 22:37:15 +0600

    path

--- src/utils/path.rs
+++ src/utils/path.rs
@@ -2,43 +2,31 @@ use std::{env, path::PathBuf};
 
 /// Returns the path to the config file.
 pub fn config_toml_path() -> PathBuf {
-    #[cfg(debug_assertions)]
-    return local().join("config.toml");
-
-    #[cfg(not(debug_assertions))]
-    config_path().join("config.toml")
+    config_base_path().join("config.toml")
 }
 
 /// Returns the path to the log file.
 pub fn log_path() -> PathBuf {
-    #[cfg(debug_assertions)]
-    return local().join("orivo.log");
-
-    #[cfg(not(debug_assertions))]
-    local_path().join("orivo.log")
+    state_base_path().join("orivo.log")
 }
 
 /// Returns the path to the database file.
 pub fn db_path() -> PathBuf {
-    #[cfg(debug_assertions)]
-    return local().join("orivo.sqlite");
-
-    #[cfg(not(debug_assertions))]
-    local_path().join("orivo.sqlite")
+    state_base_path().join("orivo.sqlite")
 }
 
 /// Returns the path to the persisted timer state file.
 pub fn store_path() -> PathBuf {
-    #[cfg(debug_assertions)]
-    return local().join("store.json");
-
-    #[cfg(not(debug_assertions))]
-    local_path().join("store.json")
+    state_base_path().join("store.json")
 }
 
 /// the base directory for runtime state files
 #[cfg_attr(debug_assertions, allow(dead_code))]
-fn local_path() -> PathBuf {
+fn state_base_path() -> PathBuf {
+    #[cfg(debug_assertions)]
+    return local();
+
+    #[cfg(not(debug_assertions))]
     PathBuf::from(home())
         .join(".local")
         .join("state")
@@ -47,7 +35,11 @@ fn local_path() -> PathBuf {
 
 /// The base directory for config files.
 #[cfg_attr(debug_assertions, allow(dead_code))]
-fn config_path() -> PathBuf {
+fn config_base_path() -> PathBuf {
+    #[cfg(debug_assertions)]
+    return local();
+
+    #[cfg(not(debug_assertions))]
     PathBuf::from(home()).join(".config").join(env!("CARGO_PKG_NAME"))
 }
 
